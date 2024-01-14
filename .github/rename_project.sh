6d77199ecfa363e1d9e4287589c0bb58ab9cff6d
manciii <manciii@users.noreply.github.com>
2024-01-14 07:58:19 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="manciii"
+original_name="auction"
+original_urlname="Auction"
+original_description="Awesome auction created by manciii"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv auction $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
